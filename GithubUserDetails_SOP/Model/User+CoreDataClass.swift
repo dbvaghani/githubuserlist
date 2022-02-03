3c31001f195f274e9cd8f0840219e1fc7b1cd099
apsl <jamesroze@hotmail.com>
2022-02-03 19:23:57 +0530

    - Initial commit

---
+++ GithubUserDetails_SOP/Model/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  GithubUserDetails_SOP
+//
+//  Created by Apsl on 02/02/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class User: NSManagedObject {
+
+}
